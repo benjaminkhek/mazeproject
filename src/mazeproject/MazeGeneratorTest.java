af73fade7f6c79db074f05aa0bd54f04f7323fee
1527thomas <thomas.wang@sjsu.edu>
2018-11-23 21:34:11 -0800

    Forgot to commit these files. Finished pt 2. Just needs comments

--- src/mazeproject/MazeGeneratorTest.java
+++ src/mazeproject/MazeGeneratorTest.java
@@ -5,11 +5,17 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class MazeGeneratorTest {
+	
 	@Test
 	void test() {
 		MazeGenerator mg = new MazeGenerator(0);
 		System.out.println("Maze 1:");
-		String[][] maze = mg.maze(4);
+		String[][] maze = mg.maze(5);
+		String[][] maze2 = new String[maze.length][maze.length];
+		for(int i = 0; i < maze.length; i++) {
+			maze2[i] = maze[i].clone();
+		}
+		
 		for(int i = 0; i < maze.length; i++){//Print out an unfinished maze.
 			for(int j = 0; j < maze.length; j++) {
 				System.out.print(maze[i][j]);
@@ -18,82 +24,46 @@ class MazeGeneratorTest {
 				}
 			}
 		}
+		
 		String[][] DFSsolvedMaze = mg.DFSSolve(maze);
 		System.out.println("DFS: ");
-		for(int i = 0; i < maze.length; i++){//Print out a maze solved using DFS
-			for(int j = 0; j < maze.length; j++) {
-				System.out.print(maze[i][j]);
+		for(int i = 0; i < DFSsolvedMaze.length; i++){//Print out a maze solved using DFS
+			for(int j = 0; j < DFSsolvedMaze.length; j++) {
+				System.out.print(DFSsolvedMaze[i][j]);
 				if(j == maze.length - 1) {
 					System.out.println("");
 				}
 			}
 		}
-		String[][] mazepath = mg.path(DFSsolvedMaze);
-		for(int i = 0; i < DFSsolvedMaze.length; i++){//Print out an unfinished maze.
-			for(int j = 0; j < DFSsolvedMaze.length; j++) {
-				System.out.print(DFSsolvedMaze[i][j]);
-				if(j == DFSsolvedMaze.length - 1) {
-					System.out.println("");
-				}
-			}
-		}
-		String[][]maze2 = mg.maze(5);
-		System.out.println("Maze 2:");
-		for(int i = 0; i < maze2.length; i++){//Print out an unfinished maze.
-			for(int j = 0; j < maze2.length; j++) {
-				System.out.print(maze2[i][j]);
-				if(j == maze2.length - 1) {
+		String[][] mazepathDFS = mg.path(DFSsolvedMaze);
+		for(int i = 0; i < mazepathDFS.length; i++){//Print out an unfinished maze.
+			for(int j = 0; j < mazepathDFS.length; j++) {
+				System.out.print(mazepathDFS[i][j]);
+				if(j == mazepathDFS.length - 1) {
 					System.out.println("");
 				}
 			}
 		}
 		
-		String[][] DFSsolvedMaze2 = mg.DFSSolve(maze2);
-		System.out.println("DFSSolve:");
-		for(int i = 0; i < maze2.length; i++){//Print out an unfinished maze.
-			for(int j = 0; j < maze2.length; j++) {
-				System.out.print(maze2[i][j]);
-				if(j == maze2.length - 1) {
+		String[][] BFSsolvedMaze = mg.BFSSolve(maze2);
+		System.out.println("BFS: ");
+		for(int i = 0; i < BFSsolvedMaze.length; i++){//Print out a maze solved using DFS
+			for(int j = 0; j < BFSsolvedMaze.length; j++) {
+				System.out.print(BFSsolvedMaze[i][j]);
+				if(j == maze.length - 1) {
 					System.out.println("");
 				}
 			}
 		}
-		String[][] mazepath2 = mg.path(DFSsolvedMaze2);
-		for(int i = 0; i < DFSsolvedMaze2.length; i++){//Print out an unfinished maze.
-			for(int j = 0; j < DFSsolvedMaze2.length; j++) {
-				System.out.print(DFSsolvedMaze2[i][j]);
-				if(j == DFSsolvedMaze2.length - 1) {
+		String[][] mazepathBFS = mg.backTrack(maze2);
+		for(int i = 0; i < mazepathBFS.length; i++){//Print out an unfinished maze.
+			for(int j = 0; j < mazepathBFS.length; j++) {
+				System.out.print(mazepathBFS[i][j]);
+				if(j == mazepathBFS.length - 1) {
 					System.out.println("");
 				}
 			}
 		}
-		
-	
-		
-	}
-	@Test
-	void test2() {
-		MazeGenerator mg = new MazeGenerator(0);
-		System.out.println("Check if mazes of size 4 are the same:");
-		String[][] maze1 = mg.maze(4);
-		for(int i = 0; i < maze1.length; i++){//Print out an unfinished maze.
-			for(int j = 0; j < maze1.length; j++) {
-				System.out.print(maze1[i][j]);
-				if(j == maze1.length - 1) {
-					System.out.println("");
-				}
-			}
-		}
-		String[][] maze2 = mg.maze(4);
-		for(int i = 0; i < maze2.length; i++){//Print out an unfinished maze.
-			for(int j = 0; j < maze2.length; j++) {
-				System.out.print(maze2[i][j]);
-				if(j == maze2.length - 1) {
-					System.out.println("");
-				}
-			}
-		}
-		assertEquals(maze1, maze2);
 	}
 	
 
